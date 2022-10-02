4317c887ac0ce34d1272cae0377aafeafc71733f
ekusiadadus <ekusiadadus@gmail.com>
2022-10-03 01:50:08 +0900

    fix(parser): add expect function

--- Parser.h
+++ Parser.h
@@ -8,18 +8,42 @@
 
 #include "Tokenizer.h"
 #include <optional>
+#include <string>
+#include <map>
 
 namespace simpleparser {
   using namespace std;
+
+  enum BUILD_IN_TYPE {
+    INT8,
+    UNIT8,
+    INT32,
+    UINT32,
+    DOUBLE,
+    STRUCT,
+    VOID,
+  };
+
+  class Type {
+  public:
+    Type(const string &name = "", BUILD_IN_TYPE type = VOID) : mName(name), mType(type) {}
+
+    string mName;
+    enum BUILD_IN_TYPE mType;
+    vector<Type> mStructMembers;
+  };
   class Parser {
   public:
+    Parser();
     void parse(vector<Token> &tokens);
 
   private:
+    optional<Type> expectType();
     optional<Token> expectIdentifier(const string &name = string());
     optional<Token> expectOperator(const string &name = string());
     vector<Token>::iterator mCurrentToken;
     vector<Token>::iterator mEndToken;
+    map<string, Type> mTypes;
   };
 
 }// namespace simpleparser
